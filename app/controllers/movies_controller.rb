f6a4ff659f13db1e46b1d4484f3d98777ae8df67
gerson <gersonrevatta@gmail.com>
2018-09-01 20:01:25 -0500

    Add restrictions for the user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :find_movie, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @movies = Movie.all.order('created_at DESC')
   end
